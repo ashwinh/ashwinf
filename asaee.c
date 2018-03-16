d050d3fece9da10f9881bc125e99d84a8fcf2f71
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-03-16 16:11:39 +0530

    Update asaee.c

--- asaee.c
+++ asaee.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
 int kelvin,celcius;
-printf("enter the celcius:");
+printf("enter the celcius:"):
 scanf("%d",&celcius);
 kelvin=celcius+273;
 printf("kelvin:%d",kelvin);
-return 0;
+getch();
 }
