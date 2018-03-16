242e835cd2a7c647da72718e28a1030fc61f093b
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-03-16 16:14:07 +0530

    Create a94.c

---
+++ a94.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int i,a[10],k,n;
+scanf("%d",&k);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+if(i=k)
+printf("%d",a[i]);
+}
+getch();
+}
