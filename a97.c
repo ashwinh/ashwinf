135037ef9b3a3735dca5ebe8c123082d85b77061
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-03-16 16:25:32 +0530

    Create a97.c

---
+++ a97.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+int i,n,count=0,rem;
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+while(n!=0)
+{
+rem=n%10;
+count++;
+n=n/10;
+printf("%d",rem);
+}  
+}	    
+return 0;
+}
